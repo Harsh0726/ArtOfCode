78abb51be3333567df9090f166e69ad3d0ed2366
MaheshaKandambi <maheshakandambi97@gmail.com>
2021-01-28 21:35:11 +0530

    repeat add file

--- particle/Home/Blog/registrationForm/php/login.php
+++ particle/Home/Blog/registrationForm/php/login.php
@@ -1,22 +1,3 @@
-<?php
-    $url='localhost';
-    $username='root';
-    $password='';
-    $conn=mysqli_connect($url,$username,$password,"fregister");
-    if(!$conn){
-        die('Could not Connect My Sql:' .mysql_error());
-    }
-
-    $sql = "CREATE TABLE register
-    (
-        `ID` int(10) NOT NULL,
-  `First_Name` varchar(100) NOT NULL,
-  `Last_Name` varchar(100) NOT NULL,
-  `Email` varchar(100) NOT NULL,
-  `Password` int(100) NOT NULL,
-  `File` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -32,94 +13,112 @@
     <!-- animation -->
     <link rel="stylesheet" href="css/tip.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    
+    <script src="javaScript/vali.js"></script>
 
 
   </head>
   <body>
+
+    
     <div class="bg"></div>
+   
     <div class="login-wrap">
         <div class="login-html">
             <input id="tab-1" type="radio" name="tab" class="sign-in" ><label for="tab-1" class="tab">Sign In</label>
              <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label> 
 
             <!-- Sign In form -->
-            <div class="login-form">
+            
+            <!-- new -->
+            <div class="login-form">  
+                
                 <div class="sign-in-htm">
+                    <div ng-app ng-init="checked = false">
+                    <form class="form-signin" action="validation.php" method="post" name="Loform">
+                    
                 
                     <!-- Input User Name -->
                     <div class="group">
                        <label for="user" class="label">User Name</label>
-                        <input id="user" type="text" placeholder="&#61447  artofcode@gmail.com" class="input">  
+                        <input id="user" type="text" placeholder= " &#61447  artofcode@gmail.com" class="input" name="email"  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" required>  
                         <small id="emailHelp" class="form-text text-muted"> We"ll never share your email with anyone else.</small>
                     </div>
 
                     <!-- Input Password -->
                     <div class="group">
                         <label for="pass" class="label">Password</label>
-                        <input id="pass" type="password" class="input" data-type="password" placeholder="&#xf084  password">
+                        <input id="pass" type="password" class="input"  data-type="password" placeholder= " &#xf084  password" name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
                     </div>
 
+                    
                     <!-- checkbox -->
-                    <div class="group">
-                        <input id="check" type="checkbox" class="check" checked>
-                        <label for="check"><span class="icon"></span> Keep me Signed in</label>
-                    </div>
+                    <input type="checkbox" id="checkbox"/>
+          <label for="checkbox" ><span class="ui"></span>Keep me signed in</label>
+          
 
                     <!-- Sign In button -->
+                    
                     <div class="group">
-                        <input type="submit" class="button" value="Sign In">
+                        <input type="submit" class="button" id="submit" value="Sign In" onclick="checkEmail();">
                     </div>
 
-                    <div class="hr"></div>
-                    <div class="foot-lnk">
+                    <div class="hr1"></div>
+                    <div class="foot-lnk1">
                         <a href="#forgot">Forgot Password?</a>
                     </div>
+
+            
+                </div>
+                </div>
                 </div>
 
+            </form>
+
                 <!-- Sign Up form -->
+                <script src="javaScript/vali.js"></script>
+                <form name="RegForm" action="registration.php" onsubmit="return validation()" method="post">
                 <div class="sign-up-htm">
-                 <form action=""
                     <div class="group">
                         <label for="user" class="label">Username</label>
-                        <input id="user" type="text" class="input" placeholder="Name">
+                        <input id="user" type="text" name="name" class="input" placeholder="Name" required>
                     </div>
                     <div class="group">
                         <label for="pass" class="label">Email Address</label>
-                        <input id="pass" type="email" class="input" placeholder="artofcode@gmail.com">
+                        <input id="pass" type="email" name="email" class="input" placeholder="artofcode@gmail.com" required>
                     </div>
                     <div class="group">
                         <label for="pass" class="label">Password</label>
-                        <input id="pass" type="password" class="input" data-type="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" placeholder="Password" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
+                        <input id="pass" type="password" name="password_1" class="input" data-type="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" placeholder="Password" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
                     </div>
                     <div class="group">
                         <label for="pass" class="label">Confirm Password</label>
-                        <input id="pass" type="password" class="input" data-type="password" placeholder="Retype Password">
+                        <input id="pass" type="password" class="input" name="password_2" data-type="password" placeholder="Retype Password" required>
                     </div>
                     
-                    <!-- <div class="group">
-                        <input type="submit" class="button" value="Sign Up">
-                    </div> -->
-
-                    <!--animation-->
                     <div id="animation">
+                        <div class="group">
+                            <p><input type="submit" class="btn-success" value="Sign Up" id="toggle"></p>
+                        </div>
 
-                        <p></p><button id="toggle" type="button" class="btn btn-success">Submited</button></p>
-    
-                        <!-- Animation -circle loader-->
-                        <div class="circle-loader">
-    
-                            <div class="checkmark draw"></div>
-                          
-                          </div>
-    
+                        <!--animation-->
+                        <!-- 
+
+                            <p></p><button value="submit" id="toggle" type="button" class="btn btn-success">Submited</button></p>-->
+        
+                            <!--Animation -circle loader-->
+                            <div class="circle-loader">
+        
+                                <div class="checkmark draw"></div>
+                            
+                            </div>
+        
                         <!-- Animation script -->
                           <script src="javaScript/jquery.min.js"></script>
                           <script src="javaScript/tip.js"></script>
     
                     </div> 
 
-                    <div class="hr"></div>
+                    <div class="hr2"></div>
                     <div class="foot-lnk">
                         <label for="tab-1">Already Member?</a>
                     </div>
@@ -139,5 +138,3 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
 </html>
-
-?>
