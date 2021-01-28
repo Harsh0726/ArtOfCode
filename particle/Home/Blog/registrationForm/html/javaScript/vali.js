544671a6d279889fa12f89c4b92a5631eed21c3c
MaheshaKandambi <maheshakandambi97@gmail.com>
2021-01-28 13:06:45 +0530

    validate email address

--- particle/Home/Blog/registrationForm/html/javaScript/vali.js
+++ particle/Home/Blog/registrationForm/html/javaScript/vali.js
@@ -30,3 +30,16 @@ function validation() {
 
     return true;
 }
+
+
+function checkEmail() {
+
+     var mail = document.forms["Logform"]["email"];
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    if (!filter.test(mail.value)) {
+    alert('Please provide a valid email address');
+    mail.focus;
+    return false;
+ }
+}
